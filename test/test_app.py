1e19a9fec9b832a2dcd9487d863342cb8d2d8f71
kebir allade <kebir.allade@insurlytech.com>
2024-10-09 10:34:32 +0200

    feat(add): tests

--- test/test_app.py
+++ test/test_app.py
@@ -1,5 +1,26 @@
-from app import add
+from test.app import add, subtract
 
 def test_add():
 
     assert add(2,3)==5
+
+def test_subtract():
+
+    assert subtract(3,2)==1
+
+def test_subtract_neg():
+
+    assert subtract(2,3)==-1
+
+def test_add_neg():
+
+    assert add(-2,3)==1
+
+def test_add_neg2():
+
+    assert add(2,-3)==-1
+
+def test_subtract_neg2():
+
+    assert subtract(-2,3)==-5
+
